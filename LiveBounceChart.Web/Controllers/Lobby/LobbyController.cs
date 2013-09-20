6c16fd2e101605943926773d123d34a00d553e82
Tagir Magomedov <tagir.magomedov@gmail.com>
2013-09-19 23:46:06 -0700

    Added LobbyControler

---
+++ LiveBounceChart.Web/Controllers/Lobby/LobbyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace LiveBounceChart.Web.Controllers.Lobby
+{
+    public class LobbyController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
