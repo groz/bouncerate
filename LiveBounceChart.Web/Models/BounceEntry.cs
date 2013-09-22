5faa602eb7340b47c7e7501753a14acb483ee16d
Tagir Magomedov <tagir.magomedov@gmail.com>
2013-09-22 14:15:41 -0700

    Added EntityFramework

---
+++ LiveBounceChart.Web/Models/BounceEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiveBounceChart.Web.Models
+{
+    public class BounceEntry
+    {
+        public TimeSpan BouncePeriod { get; set; }
+        public DateTime UtcExitTime { get; set; }
+    }
+}
